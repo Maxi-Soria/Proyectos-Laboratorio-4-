0fa784f8576bed3f0a7e9131c81c9f8bf0d1a81c
Maximiliano Facundo Soria <107376486+Maxi-Soria@users.noreply.github.com>
2024-04-21 18:50:25 -0300

    Tercer TP, primer aporte.

---
+++ TP 3/TP3_GRUPO_3/src/ejercicio1/UsuarioInvalidoException.java
@@ -0,0 +1,17 @@
+package ejercicio1;
+
+
+public class UsuarioInvalidoException extends RuntimeException {
+
+	private final String mensajeError = "Usuario invalido" ;
+
+	@Override
+	public String getMessage() {
+		
+		return mensajeError;
+	}
+	
+	
+	
+}
+
